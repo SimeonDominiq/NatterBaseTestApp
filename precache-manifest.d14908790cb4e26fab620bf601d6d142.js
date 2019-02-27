15f523679b874c7d50751e54cdd047b56cabe60f
Simeon Dominiq <simeondominiq@gmail.com>
2019-02-27 20:07:57 +0100

    Updates

---
+++ precache-manifest.d14908790cb4e26fab620bf601d6d142.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "843642969167a282af30",
+    "url": "/NatterBaseTestApp/static/js/runtime~main.84364296.js"
+  },
+  {
+    "revision": "dafb0c74726099ab5c5b",
+    "url": "/NatterBaseTestApp/static/js/main.dafb0c74.chunk.js"
+  },
+  {
+    "revision": "17c3ddbae7090543529b",
+    "url": "/NatterBaseTestApp/static/js/1.17c3ddba.chunk.js"
+  },
+  {
+    "revision": "dafb0c74726099ab5c5b",
+    "url": "/NatterBaseTestApp/static/css/main.a76574af.chunk.css"
+  },
+  {
+    "revision": "41445a03f35b52dabf331bc5cf62a76f",
+    "url": "/NatterBaseTestApp/index.html"
+  }
+];
